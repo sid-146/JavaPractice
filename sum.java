0e389f156b7c32cb01d0b2e4331d516ed2c1bd46
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-10 11:53:40 +0530

    Created Program

---
+++ sum.java
@@ -0,0 +1,15 @@
+import Scanner;
+
+class Sum{
+    public static void main(string[] args)
+    {
+        Scanner sc = new Scanner();
+
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+
+        int Third  = first + second;
+        System.out.println(Third);
+    
+    }
+}
