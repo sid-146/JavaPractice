e4e15d241411cbffeca7b9a7bc49010e9ae9a7f2
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-10 21:32:37 +0530

    Update RevAndSum.java

--- RevAndSum.java
+++ RevAndSum.java
@@ -5,14 +5,21 @@ class RevAndSum{
     {
         Scanner sc = new Scanner(System.in);
 
-        int Number, Reverse = 0; 
+        int Number, Reverse = 0, Addition; 
         Number = sc.nextInt();
 
         // int i = 0;
+
         while(Number >= 0)
         {  
-            Reverse = ;
-            
+            int temp = Number % 10;
+            Reverse = Reverse + temp;
+            Addition = Addition + temp;
+            Addition = Addition * 10;
+            Number = Number / 10;
         }
+
+        System.out.println(Addition);
+        System.out.println(Reverse);
     }
 }
